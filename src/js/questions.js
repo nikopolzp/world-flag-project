b8f62043e95362e487121db80312306d2ea84e3a
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-03-26 00:23:44 +0200

    add background and nav buttons

---
+++ src/js/questions.js
@@ -0,0 +1,62 @@
+export const questions = [
+  {
+    question: "Флаг якої країни?",
+    answer: "Австралія",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Australia.svg/240px-Flag_of_Australia.svg.png",
+    options: ["Австрія", "Нова Зеландія", "Коморські острови", "Австралія"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/180px-Flag_of_Austria.svg.png",
+    answer: "Австрія",
+    options: ["Австрія", "Латвія", "Перу", "Таїланд"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Flag_of_Azerbaijan.svg/240px-Flag_of_Azerbaijan.svg.png",
+    answer: "Азербайджан",
+    options: ["Лівія", "Азербайджан", "Узбекистан", "Пакистан"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/168px-Flag_of_Albania.svg.png",
+    answer: "Албанія",
+    options: ["Чорногорія", "Андора", "Албанія", "Північна Македонія"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/180px-Flag_of_Algeria.svg.png",
+    answer: "Алжир",
+    options: ["Алжир", "Ангола", "Мавританія", "Лівія"],
+  },
+  {
+    question: "Флаг якої країни?",
+    answer: "Ангола",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Flag_of_Angola.svg/180px-Flag_of_Angola.svg.png",
+    options: ["Пакистан", "Ангола", "Сінгапур", "Туреччина"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Andorra.svg/172px-Flag_of_Andorra.svg.png",
+    answer: "Андора",
+    options: ["Румунія", "Молдова", "Андора", "Східний Тімор"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Flag_of_Antigua_and_Barbuda.svg/180px-Flag_of_Antigua_and_Barbuda.svg.png",
+    answer: "Антигуа і Барбуда",
+    options: ["Вануату", "Антигуа і Барбуда", "Гаяна", "Пакистан"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/192px-Flag_of_Argentina.svg.png",
+    answer: "Аргентина",
+    options: ["Аргентина", "Ботствана", "Гватемала", "Сальвадор"],
+  },
+  {
+    question: "Флаг якої країни?",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_the_Taliban.svg/180px-Flag_of_the_Taliban.svg.png",
+    answer: "Афганистан",
+    options: ["Ірак", "Лівія", "Саудівська аравія", "Афганистан"],
+  },
+];
